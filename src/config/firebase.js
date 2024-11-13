7d86338ea09c0fdd61bc5cde40d0f4247e66abb4
ayesha-qy <88137012+ayesha-qy@users.noreply.github.com>
2024-11-13 00:40:35 -0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC6kkz9yNjthTzu8vGULBRafD-4B1Hnc_o",
-  authDomain: "fypp-73cd2.firebaseapp.com",
-  projectId: "fypp-73cd2",
-  storageBucket: "fypp-73cd2.appspot.com",
-  messagingSenderId: "1044676421219",
-  appId: "1:1044676421219:web:0c0ce6c2d11fb32010fa56",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
